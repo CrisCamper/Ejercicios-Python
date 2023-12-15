1ce35034fe16df48b99d53452eeb110f9339ef96
osfa25 <fabianmantilla98@gmail.com>
2023-12-15 17:17:46 -0500

    asteriscos

---
+++ EjerciciosPython/logica media/asteriscos.py
@@ -0,0 +1,20 @@
+# Dibujar asteriscos
+# Entrada: altura y ancho
+# Salida: Figura
+
+#print('cuadrado/rectángulo')
+#h = int(input('Ingrese altura: '))
+#a = int(input('Ingrese ancho: '))
+#
+#for i in range(h):
+#    print('*'*a)
+
+#print('triángulo')
+#h = int(input('Ingrese altura: '))
+#for i in range (h+1):
+#    temp = 0+i
+#    print('*'*temp)
+
+a = 3
+
+print()
