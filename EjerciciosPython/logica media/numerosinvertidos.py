04b2e1537eab0f0e7a7fbf8073d05d1b3e434bd8
CrisCamper <pandeabono2@gmail.com>
2023-12-16 12:30:22 -0500

    commit inverting numbers

---
+++ EjerciciosPython/logica media/numerosinvertidos.py
@@ -0,0 +1,8 @@
+# Program inverting numbers
+
+# the user input a number
+n = str(input('Input a number: '))
+
+# Make the process for inverting number 
+print(n[::-1]) # This make that list prints in reverse 
+
