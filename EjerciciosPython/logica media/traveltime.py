9b120e73149af5fb336aeafe74d2ec646acf906f
CrisCamper <pandeabono2@gmail.com>
2023-12-16 12:48:07 -0500

    ...

---
+++ EjerciciosPython/logica media/traveltime.py
@@ -0,0 +1,6 @@
+# program to calculate the time of a journey
+# ask the leg of the travel
+# the program stops asking when the user gives a zero
+# send like a result the time in hours 
+
+print(80%60)
