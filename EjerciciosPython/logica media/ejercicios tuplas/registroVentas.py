a629ba7ed95dfb72e3fefdcfb11ffed5a86e3016
osfa25 <fabianmantilla98@gmail.com>
2023-12-21 15:38:37 -0500

    renombrados:     EjerciciosPython/logica media/desviacionestandar.py -> EjerciciosPython/logica media/ejercicios listas/desviacionestandar.py 	renombrados:     EjerciciosPython/logica media/mayoresPromedio.py -> EjerciciosPython/logica media/ejercicios listas/mayoresPromedio.py 	nuevos archivos: EjerciciosPython/logica media/ejercicios tuplas/registroVentas.py

---
+++ EjerciciosPython/logica media/ejercicios tuplas/registroVentas.py
@@ -0,0 +1,10 @@
+# programa registro y productos unicos
+ventas = [
+    ("ProductoA", 10, 150),
+    ("ProductoB", 5, 200),
+    ("ProductoA", 8, 120),
+    ("ProductoC", 12, 80),
+    ("ProductoB", 3, 210),
+    ("ProductoA", 15, 130),
+    ("ProductoC", 7, 85),
+]
