15f28af45df4ce2bf6a849d292a3b64267c915f3
CrisCamper <pandeabono2@gmail.com>
2023-12-28 15:39:24 -0500

    Changes to be committed: 	new file:   EjerciciosPython/logica media/Diccionarios/aIntroDiccionarios.py 	renamed:    EjerciciosPython/logica media/ejercicios listas/aintroListas.py -> EjerciciosPython/logica media/Listas/aintroListas.py 	renamed:    EjerciciosPython/logica media/ejercicios listas/desviacionestandar.py -> EjerciciosPython/logica media/Listas/desviacionestandar.py 	renamed:    EjerciciosPython/logica media/ejercicios listas/mayoresPromedio.py -> EjerciciosPython/logica media/Listas/mayoresPromedio.py 	renamed:    EjerciciosPython/logica media/ejercicios tuplas/aIntroTuples.py -> EjerciciosPython/logica media/Tuplas/aIntroTuples.py 	renamed:    EjerciciosPython/logica media/ejercicios tuplas/registroVentas.py -> EjerciciosPython/logica media/Tuplas/registroVentas.py

---
+++ EjerciciosPython/logica media/Diccionarios/aIntroDiccionarios.py
@@ -0,0 +1,46 @@
+#DICTIONARY#
+# Example of Dictionary mix the real life with python
+
+    #Play: -------> Password
+    #Do something with joy in order to entertain yourself, have fun or develop certain abilities -------> value
+
+#Making Dictionaries
+# To create a dictionary we use keys {} around assignments password: value that are separated by commas
+
+    #empty_dict = {}
+
+    #rae = {
+        #'bifronte': 'De dos frentes o dos caras',
+        #'anarcoide': 'Que tiende al desorden',
+        #'montuvio': 'Campesino de la costa'
+    #}
+    #population_can = {
+        #2015: 2_234_546,
+        #2016: 2_548_786,
+        #2017: 2_890_234
+    #}
+    
+    #print(f'{rae}\n{population_can}') # Print rae and population_can
+
+#Exercise: make a dictionary with names and ages of 5 members of your family
+
+    #family = {
+        #'Kimberlyn': 30,
+        #'Alvaro': 57,
+        #'Nicolás': 8,
+        #'Cristopher': 17,
+        #'Lucas': 9
+    #}
+
+    #print(family)
+ 
+#OPERATIONS WITH DICTIONARIES
+#Obtain a element
+
+rae = {
+    'bifronte': 'De dos frentes o dos caras',
+    'anarcoide': 'Que tiende al desorden',
+    'montuvio': 'Campesino de la costa'
+    }
+
+print(rae['anarcoide']) # Que tiende al desorden
