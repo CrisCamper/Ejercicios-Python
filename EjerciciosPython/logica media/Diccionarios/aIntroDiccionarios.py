b5528f8dbfc826bdb54637bbf6966bfa6397f898
CrisCamper <pandeabono2@gmail.com>
2023-12-30 13:35:16 -0500

    new file:   EjerciciosPython/logica media/Conjuntos/aIntroConjuntos.py 	modified:   EjerciciosPython/logica media/Diccionarios/aIntroDiccionarios.py

--- EjerciciosPython/logica media/Diccionarios/aIntroDiccionarios.py
+++ EjerciciosPython/logica media/Diccionarios/aIntroDiccionarios.py
@@ -37,10 +37,50 @@
 #OPERATIONS WITH DICTIONARIES
 #Obtain a element
 
-rae = {
-    'bifronte': 'De dos frentes o dos caras',
-    'anarcoide': 'Que tiende al desorden',
-    'montuvio': 'Campesino de la costa'
-    }
+    #rae = {
+        #'bifronte': 'De dos frentes o dos caras',
+        #'anarcoide': 'Que tiende al desorden',
+        #'montuvio': 'Campesino de la costa'
+        #}
 
-print(rae['anarcoide']) # Que tiende al desorden
+    #print(rae['anarcoide']) # Que tiende al desorden
+
+
+#Using get()
+#If the key exist, it return us its value
+#If the key does not exist, it returns 'None' or another value that we assign to it
+
+    #rae = {
+        #'bifronte': 'De dos frentes o dos caras',
+        #'anarcoide': 'Que tiende al desorden',
+        #'montuvio': 'Campesino de la costa'
+    #}
+
+    #print(rae.get('bifronte')) # De dos frentes o dos caras
+    #print(rae.get('programación')) # None
+    #print(rae.get('programación', 'No disponible')) # No disponible
+
+#Add or Modify a element
+# To add an element to the dictionary simply reference the key and add a value to it
+
+    #rae = {
+    #    'bifronte': 'De dos frentes o dos caras',
+    #    'anarcoide': 'Que tiende al desorden',
+    #    'montuvio': 'Campesino de la costa'
+    #}
+    
+    #rae['jugar'] = 'Hacer algo divertido con el fin de entretenerse' # Add 'jugar' to diccionary
+    #print(rae)
+    #rae['Jugar'] = 'Realizar actividades divertidas para entretenerse' # Change the value of 'jugar' for another
+    #print(rae)
+
+#Creating from empty
+
+    #VOWELS = 'aeiou'
+    #enum_vowels = {}
+    #
+    #for i, vowel in enumerate(VOWELS, start=1):
+    #    enum_vowels[vowel] = i
+    #
+    #print(enum_vowels)
+
