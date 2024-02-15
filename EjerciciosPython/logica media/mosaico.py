cec7d3e56195db028326aefb21fb8ad78e780578
Cristopher Daniel Ramirez Buitrago <145687056+CrisCamper@users.noreply.github.com>
2024-02-15 16:24:32 -0500

    Delete EjerciciosPython directory

--- EjerciciosPython/logica media/mosaico.py
+++
@@ -1,9 +0,0 @@
-#diagonal principal : X
-#parte inferior : D
-#parte superior : U
-#obtener tamaño
-
-size = int(input('Input size: '))
-
-for i in range(1, (size+1)):
-    print('D '*(i-1)+'X '+'U '*(size-i))
--- EjerciciosPython/logica media/numerosinvertidos.py
+++
@@ -1,8 +0,0 @@
-# Program inverting numbers
-
-# the user input a number
-n = str(input('Input a number: '))
-
-# Make the process for inverting number 
-print(n[::-1]) # This make that list prints in reverse 
-
